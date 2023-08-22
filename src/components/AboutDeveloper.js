b212643f3452a6ff4345f82af39426f905471f4b
SameerMughal(Dark) <mughalsameer007@gmail.com>
2023-08-22 18:13:41 +0500

    fixing about us image issue now it is static image not dynamic

--- src/components/AboutDeveloper.js
+++ src/components/AboutDeveloper.js
@@ -1,17 +1,19 @@
+import developerImg from '../images/devImage.jpg'
 function AboutDeveloper(props) {
+    
 
-    let developerPictureLink = 'https://media.licdn.com/dms/image/D4D03AQEU2M0O0eGneg/profile-displayphoto-shrink_800_800/0/1671514154451?e=1688601600&v=beta&t=y0USMnNKYDQDgSBNxjVh1RvAkZ6miQ6mUL3o5hApuL8';
+   
     // creating function to open Image in new tab
     let openInNewTab = () => {
         console.log('Clicking');
-        window.open(developerPictureLink, '_blank', 'noreferrer');
+        window.open(developerImg, '_blank', 'noreferrer');
 
     }
     return (
         <div className="container my-3" style={props.themeColor==='dark'?{backgroundColor:'#182028',color:'white'}:{color:'black',backgroundColor:'white'}} >
             <h1 >About-Us</h1>
             <div className="card" style={props.themeColor==='dark'?{backgroundColor:'#182028',color:'white'}:{color:'black',backgroundColor:'white'}} >
-                <img src={developerPictureLink} onClick={openInNewTab} className="card-img-top" alt="Developer " style={{ width: '200px', cursor: 'pointer' }} />
+                <img src={developerImg} onClick={openInNewTab} className="card-img-top" alt="Developer " style={{ width: '200px', cursor: 'pointer' }} />
                 <div className="card-body" >
                     <h5 className="card-title">{props.developerName}</h5>
                     <p className="card-text">I Develop this web-app just for fun and it is also usefull. Because it is use to modified your text characteristics like to change cases of text or counting the words and letters and much more things like that'</p>
